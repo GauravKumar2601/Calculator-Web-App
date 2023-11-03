1febecc5d6f0cdcebb624257a18c56e065f35e0a
GauravKumar260100 <gaurav.kumar.mcs21@cs.du.ac.in>
2023-11-03 17:00:09 +0530

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body);
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+
+  let result = num1 + num2;
+  if (!isNaN(result)) {
+    res.send("The result is " + result);
+  }
+});
+
+app.listen(3005, function () {
+  console.log("Server is running on port: " + 3005);
+});
